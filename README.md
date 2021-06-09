5cb7e9fa42122e2e0df173bad723de070ec72204
stud-create <81812386+stud-create@users.noreply.github.com>
2021-06-09 13:50:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# p35final
+# afa156b839e1b81fbfa9bf61b99ed6eb
---
+++ sketch.js
@@ -0,0 +1,67 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database,position
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+  var balloonPosition=database.ref('balloon/height')
+  balloonPosition.on("value",readPosition,showError)
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+  if(keyDown(LEFT_ARROW)){
+  updateHeight(-10,0);
+  balloon.addAnimation("image",balloonImage2)
+ }
+ else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    balloon.addAnimation("image",balloonImage2)
+ }
+ else if(keyDown(UP_ARROW)){
+updateHeight(0,-10);
+balloon.addAnimation("image",balloonImage2)
+balloon.scale=balloon.scale-0.005
+ }
+ else if(keyDown(DOWN_ARROW)){
+  updateHeight(0,+10);
+  balloon.addAnimation("image",balloonImage2)
+  balloon.scale=balloon.scale+0.005
+}
+ 
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+ }
+function updateHeight(x,y){
+  database.ref('balloon/height').set({
+   'x': position.x + x ,
+   'y': position.y + y 
+  })
+}
+function readPosition(data){
+ position = data.val();
+  balloon.x = position.x;
+  balloon.y = position.y;
+}
+function showError(){
+  console.log("Error in writing to the database")
+}
